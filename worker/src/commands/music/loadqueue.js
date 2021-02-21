07ccb97a1a3e13a285ff16572eb63668c4a9457d
diced <pranaco2@gmail.com>
2021-02-21 08:59:45 -0800

    queue saving/loading

---
+++ worker/src/commands/music/loadqueue.js
@@ -0,0 +1,31 @@
+const Command = require('../../structures/Command');
+
+module.exports = class extends Command {
+  constructor(client) {
+    super({
+      name: 'loadqueue',
+      interaction: true
+    }, client);
+  }
+
+  async exec(ctx, args) {
+    const state = await ctx.voiceState();
+    if (!state) return ctx.send({ content: 'Please join a voice channel.' });
+
+    const player = await ctx.player();
+    const queue = args.single();
+    if (!queue) return ctx.send({ content: 'No queue found' });
+    const user = await this.client.util.user(ctx.user.id);
+    if (!user.saved[queue]) return ctx.send({ content: 'No queue found' });
+
+    for (const t of user.saved[queue]) player.queue.push(t);
+
+    await player.connect(state.channel_id);
+
+    if (player.playing) return ctx.send({ content: `Added **${user.saved[queue].length}** songs from **${queue}**` });
+    else {
+      player.startPlaying(ctx, player.queue[0]);
+      return ctx.send({ content: `Playing **${user.saved[queue].length}** songs from **${queue}**` });
+    }
+  }
+};
---
+++ worker/src/commands/music/savequeue.js
@@ -0,0 +1,28 @@
+const Command = require('../../structures/Command');
+
+module.exports = class extends Command {
+  constructor(client) {
+    super({
+      name: 'savequeue',
+      interaction: true
+    }, client);
+  }
+
+  async exec(ctx, args) {
+    const state = await ctx.voiceState();
+    if (!state) return ctx.send({ content: 'Please join a voice channel.' });
+
+    const player = await ctx.player(false);
+    if (!player) return ctx.send({ content: 'No player active at the moment.' });
+
+    const queue = args.single();
+    if (!queue) return ctx.send({ content: 'No queue name specified.' });
+    if (!player.queue.length) return ctx.send({ content: 'No songs in current queue.' });
+
+    const user = await this.client.util.user(ctx.user.id);
+    user.saved[queue] = player.queue;
+
+    await this.client.pg.user.save(user);
+    return ctx.send({ content: `Saved queue to **${queue}**` });
+  }
+};
