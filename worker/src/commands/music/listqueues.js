a6676d41dc7eac182f8eadb4bdfb2303680566c0
diced <pranaco2@gmail.com>
2021-02-21 21:26:43 -0800

    list que

---
+++ worker/src/commands/music/listqueues.js
@@ -0,0 +1,17 @@
+const Command = require('../../structures/Command');
+
+module.exports = class extends Command {
+  constructor(client) {
+    super({
+      name: 'listqueues',
+      aliases: ['list'],
+      interaction: true
+    }, client);
+  }
+
+  async exec(ctx) {
+    const saved = Object.keys((await this.client.util.user(ctx.user.id)).saved);
+    if (!saved.length) return ctx.send({ content: 'You don\'t have any saved queues.' });
+    return ctx.send({ content: `Queues: ${saved.map(x=>`\`${x}\``).join(', ')}` })
+  }
+};
