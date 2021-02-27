dc3457460a5dacfc298f26934d66ea165843c00b
diced <pranaco2@gmail.com>
2021-02-27 09:51:41 -0800

    check if ./protobuf exists

--- gateway/build.rs
+++ gateway/build.rs
@@ -1,12 +1,19 @@
+use std::path::Path;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let path = match Path::new("protobuf").exists() {
+    true => "protobuf",
+    false => "../protobuf"
+  };
+
   tonic_build::configure().compile(
     &["discord/v1/model.proto", "discord/v1/cache.proto"],
-    &["pylon-gateway-protobuf"],
+    &[path],
   )?;
 
   tonic_build::configure().compile(
     &["gateway/v1/cache_service.proto"],
-    &["pylon-gateway-protobuf"],
+    &[path],
   )?;
   Ok(())
 }
