c04e7237f6a83744b6d9a35b1242bac083067be8
diced <pranaco2@gmail.com>
2021-03-01 22:26:32 -0800

    starting stats

---
+++ worker/src/commands/system/stats.js
@@ -0,0 +1,21 @@
+const Command = require('../../structures/Command');
+
+module.exports = class extends Command {
+  constructor(client) {
+    super({
+      name: 'stats',
+      description: 'Get the stats of the bot'
+    }, client);
+  }
+
+  async exec(ctx) {
+    const memory = process.memoryUsage();
+
+    return ctx.embed({
+      title: 'Stats',
+      fields: [
+        { name: 'Worker', value: `**Heap Used:** ${(memory.heapUsed / 1024 / 1024).toFixed(2)}\n**RSS:** ${(memory.rss / 1024 / 1024).toFixed(2)}\n`, inline: true }
+      ]
+    });
+  }
+};
